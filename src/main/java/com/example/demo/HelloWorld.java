f19079fce289bd47c5fc65637370e7b7177935a9
夏肇真 <422645940@qq.com>
2017-11-17 09:20:29 +0800

    springBootTest

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by sgl on 17/11/17.
+ */
+@Controller
+public class HelloWorld {
+
+
+    @ResponseBody
+    @RequestMapping(value = "helloworld",method = RequestMethod.GET)
+    public String helloWorld(){
+        return "hello world";
+    }
+}
